90aa6965353eb36b2e3f7a50f92257c9dfb63145
Kushal Chakrabarti <kushalc@talent.works>
2018-02-21 13:55:00 -0800

    Fixed up paths.

--- runner.py
+++ runner.py
@@ -2,6 +2,7 @@
 
 from NLTKMaxentEthnicityClassifier import NLTKMaxentEthnicityClassifier as mxec
 import sys, io, os
+from .setup import PACKAGE_NAME
 import cPickle as pickle
 
 def unpickle(string):
@@ -9,14 +10,17 @@ def unpickle(string):
 	to_ret = pickle.load(pickle_file)
 	return to_ret
 
+ETHNICITY_PKL = "pickled_classifiers/danish_irish_chinese_czech_japanese_french_jewish_indian_spanish_italian_.pkl"
 def make_classifier():
-	return unpickle('src/ethnicity/pickled_classifiers/danish_irish_chinese_czech_japanese_french_jewish_indian_spanish_italian_.pkl')
+	# Can't use pkg_resources because the pickled files don't get installed for some reason. -e?
+	path = os.path.join(os.environ["APP_ROOT"], "src", PACKAGE_NAME, ETHNICITY_PKL)
+	clf = unpickle(path)
+	return clf
 
 def main():
 	classifier = make_classifier()
-	# classifier = unpickle('src/ethnicity/pickled_classifiers/danish_irish_chinese_czech_japanese_french_jewish_indian_spanish_italian_.pkl')
 	print classifier
-	while(True):
+	while (True):
 		name = raw_input("Enter a name (enter to quit) -->: ")
 		if name == "":
 			break
--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import find_packages, setup
+PACKAGE_NAME = "ethnicity_guesser"
 
-setup(
-    name         = 'ethnicity-guesser',
-    version      = '0.1',
-    packages     = ['ethnicity_guesser'],
-    package_dir  = {
-        'ethnicity_guesser': '.',
-    },
-    package_data = {
-        'ethnicity_guesser':   ['./*'],
-    },
-)
+if __name__ == "__main__":
+    setup(
+        name         = 'ethnicity-guesser',
+        version      = '0.1',
+        packages     = [PACKAGE_NAME],
+        package_dir  = {
+            PACKAGE_NAME: '.',
+        },
+        package_data = {
+            PACKAGE_NAME: ['./*'],
+        },
+    )

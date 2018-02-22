f55bbf2ece9f11d95317c3faa6efaa4d823937ca
Kushal Chakrabarti <kushalc@talent.works>
2018-02-22 03:14:03 -0800

    Switched to cloudpickle.

--- runner.py
+++ runner.py
@@ -2,27 +2,28 @@
 
 import sys, io, os
 import pkg_resources
-import cPickle as pickle
+import cloudpickle
 
 from NLTKMaxentEthnicityClassifier import NLTKMaxentEthnicityClassifier as mxec
 from setup import PACKAGE_NAME
 
 def unpickle(string):
 	pickle_file = open(string, 'rb')
-	to_ret = pickle.load(pickle_file)
+	to_ret = cloudpickle.load(pickle_file)
 	return to_ret
 
-ETHNICITY_PKL = "pickled_classifiers/combined.pkl"
+ETHNICITY_PKL = "pickled_classifiers/combined.cloudpickle"
 def make_classifier():
 	path = pkg_resources.resource_filename(PACKAGE_NAME, ETHNICITY_PKL)
 	if not os.path.exists(path):
 		all_toks = []
 		for pickle_file in os.listdir("pickled_names"):
 			all_toks.append(unpickle('pickled_names/' + pickle_file))
+
 		clf = mxec(all_toks)
 		clf.train()
 
-		pickle.dump(clf, open(path, "wb"))
+		cloudpickle.dump(clf, open(path, "wb"))
 
 	else:
 		clf = unpickle(path)
